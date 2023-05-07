3f432d58d66087c18d914c7a3096b685a67caffd
Eugene Zanevskij <zanevskijzenia@gmail.com>
2023-05-07 17:32:35 +0300

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,44 @@
+const path = require('path');
+const fs = require('fs');
+const { rm, mkdir, readdir, copyFile } = require('fs/promises');
 
+const fromFolder = path.join(__dirname, 'files');
+const toFolder = path.join(__dirname, 'files-copy');
+
+const copyDir = async (from, to) => {
+  try {
+    await deleteFolder(to);
+  } catch {} 
+  finally {
+    await createFolder(to);
+    const files = await getFilesInfo(from);
+    await copyFiles(files, from, to);
+  }
+};
+
+const deleteFolder = async (folder) => {
+  await rm(folder, { recursive: true });
+};
+
+const createFolder = async (folder) => {
+  await mkdir(folder, { recursive: true });
+};
+
+const getFilesInfo = async (folder) => {
+  const files = await readdir(folder, {withFileTypes: true});
+  return files;
+};
+
+const copyFiles = async (files, fromFolder, toFolder) => {
+  for (let file of files) {
+    const fromFolderFile = path.join(fromFolder, file.name);
+    const toFolderFile = path.join(toFolder, file.name);
+    if (file.isFile()) {
+      copyFile(fromFolderFile, toFolderFile);
+    } else {
+      await copyDir(fromFolderFile, toFolderFile);
+    }
+  }
+};
+
+copyDir(fromFolder, toFolder);
